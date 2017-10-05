a358494133761023d78364023eb6d3921b7bac21
synedraacus <alexeymorozov1991@gmail.com>
2017-10-05 16:25:02 +0800

    Collision check function & util tests

--- bear_utilities.py
+++ bear_utilities.py
@@ -33,3 +33,22 @@ def copy_shape(l, value=None):
         else:
             r.append(value)
     return r
+
+
+def rectangles_collide(pos1, size1, pos2, size2):
+    """
+    Return True if the rectangles collide
+    Rectangles are supplied in [x,y], [xsize, ysize] form with the left corner
+    and size. Assumes positions and sizes to be sorted
+    :param pos1: 
+    :param size1:
+    :param pos2:
+    :param size2:
+    :return: 
+    """
+    # X overlap
+    if pos1[0] <= pos2[0]+size2[0]-1 and pos2[0] <= pos1[0]+size1[0]-1:
+        # Y overlap
+        if pos1[1] <= pos2[1]+size2[1]-1 and pos2[1] <= pos1[1]+size1[1]-1:
+            return True
+    return False
---
+++ tests/test_utilities.py
@@ -0,0 +1,16 @@
+# Tests for bear_utilities
+
+from bear_utilities import *
+
+
+def test_shapes():
+    l = [[1, 1], [2, 2], [3, 3, 3]]
+    l2 = copy_shape(l, None)
+    assert l2 == [[None, None], [None, None], [None, None, None]]
+    assert shapes_equal(l, l2)
+
+
+def test_collision():
+    assert rectangles_collide((5, 5), (2, 2), (5, 6), (1, 1))
+    assert rectangles_collide((5, 5), (2, 2), (1, 1), (5, 10))
+    assert not rectangles_collide((5, 5), (1, 1), (6, 6), (1, 1))
