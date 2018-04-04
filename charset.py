88ff72138dfb07a0104892e648811ed36f2af226
SynedraAcus <alexeymorozov1991@gmail.com>
2018-04-04 20:55:40 +0800

    A demo for character issue

---
+++ charset.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3.6
+
+from bear_hug import BearTerminal, BearLoop
+from widgets import Widget
+from resources import XpLoader
+from event import BearEventDispatcher
+
+terminal = BearTerminal(size='18x18', title='Charset issue',
+                        filter=['keyboard', 'mouse'])
+terminal.start()
+loop = BearLoop(terminal, BearEventDispatcher())
+loader = XpLoader(filename='charlist.xp')
+w = Widget(*loader.get_image())
+terminal.add_widget(w, refresh=True)
+loop.run()
