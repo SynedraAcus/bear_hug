af6e9e23616f1c981553dd304d09fdf459f454d0
SynedraAcus <alexeymorozov1991@gmail.com>
2018-04-18 21:07:00 +0800

    Edited setup files

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from setuptools import setup
 setup(
   name = 'bear_hug',
-  packages = ['bear_hug'], # this must be the same as the name above
-  version = '0.0.1',
-  description = 'An object-oriented ECS library for ASCII games. Wraps bearlibterminal',
+  packages = ['bear_hug'],
+  version = '0.1',
+  description = 'An object-oriented ECS library for ASCII games. Wraps bearlibterminal.',
   author = 'Alexey Morozov',
   author_email = 'alexeymorozov1991@gmail.com',
-  url = 'https://github.com/SynedraAcus/bear_hug', # use the URL to the github repo
-  download_url = 'https://github.com/SynedraAcus/bear_hug/archive/0.0.1.tar.gz', # I'll explain this in a second
+  url = 'https://github.com/SynedraAcus/bear_hug',
+  download_url = 'https://github.com/SynedraAcus/bear_hug/archive/0.0.1.tar.gz',
   keywords = ['gamedev', 'ascii', 'ASCII'],
-  license='MIT',
+  license = 'MIT',
+  python_requires = '>=3.6',
   install_requires=['bearlibterminal'],
   classifiers = [],
 )
