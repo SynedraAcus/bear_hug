e9e4a2ed17b2fd29b0ae20b951a8dcbfdb3905a1
SynedraAcus <alexeymorozov1991@gmail.com>
2019-01-27 00:23:38 +0800

    SwitchingWidget

--- bear_hug/widgets.py
+++ bear_hug/widgets.py
@@ -112,6 +112,43 @@ class Widget:
             self.colors = self.colors[::-1]
 
 
+class SwitchingWidget(Widget):
+    """
+    A widget that can contain a collection of chars/colors pairs and switch
+    them on command. These char/color pairs should all be the same shape.
+    Does not do any transition animations.
+    """
+    
+    def __init__(self, images_dict, initial_image=None):
+        test_shape = None
+        for image in images_dict:
+            if not shapes_equal(images_dict[image][0], images_dict[image][1]):
+                raise BearException(
+                    f'Chars and colors of different shape for image ID {image} in SwitchingWidget')
+            if not test_shape:
+                test_shape = (len(images_dict[image][0]),
+                              len(images_dict[image][0][0]))
+            elif len(images_dict[image][0]) != test_shape[0] or \
+                    len(images_dict[image][0][0]) != test_shape[1]:
+                raise BearException(
+                    f'Image {image} in SwitchingWidget has incorrect size')
+        if not initial_image:
+            raise BearException('Initial image not set for SwitchingWidget')
+        super().__init__(*images_dict[initial_image])
+        self.images = images_dict
+        self.current_image = initial_image
+    
+    def switch_to_image(self, image_id):
+        if image_id != self.current_image:
+            try:
+                self.chars = self.images[image_id][0]
+                self.colors = self.images[image_id][1]
+                self.current_image = image_id
+            except KeyError:
+                raise BearException(
+                    f'Attempting to switch to incorrect image ID {image_id}')
+            
+            
 class Layout(Widget):
     """
     A widget that can add others as its children.
