bdb79fd036c4a5897be253735b7327c1d4347699
synedraacus <alexeymorozov1991@gmail.com>
2019-09-23 21:13:29 +0800

    setup.py typo fix

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup
 setup(
   name = 'bear_hug',
   packages = ['bear_hug'],
-  version = '0.1.2',
+  version = '0.2',
   description = 'An object-oriented ECS library for ASCII games. Wraps bearlibterminal.',
   author = 'Alexey Morozov',
   author_email = 'alexeymorozov1991@gmail.com',
   url = 'https://github.com/SynedraAcus/bear_hug',
-  download_url = 'https://github.com/SynedraAcus/bear_hug/archive/0.1.1.tar.gz',
+  download_url = 'https://github.com/SynedraAcus/bear_hug/archive/v0.2.tar.gz',
   keywords = ['gamedev', 'ascii', 'ASCII'],
   license = 'MIT',
   python_requires = '>=3.6',
-  install_requires=['bearlibterminal'],
+  install_requires=['bearlibterminal', 'simpleaudio'],
   classifiers = [],
 )
